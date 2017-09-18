952faee08c1cf35eba04d06622ee4888acd503aa
Kitty Pelerin <KPelerin78@gmail.com>
2017-09-18 00:59:05 -0400

    react form Homework

--- quotes-form-start/src/App.js
+++ quotes-form-start/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuotesList from './components/QuotesList';
+import AddQuoteForm from './components/AddQuoteForm';
 import axios from 'axios';
 import logo from './millie.png';
 import './App.css';
@@ -8,61 +9,116 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      apiData: [],
-      bgColor: '#FFFFFF'
+      quotes: [],
+      bgColor: '#ffb3b3',
+      inputContentValue: '',
+      inputAuthorValue: '',
+      inputGenreValue: '',
     }
     this.changeBgColor = this.changeBgColor.bind(this);
+
+    this.handleInputContentChange = this.handleInputContentChange.bind(this);
+    this.handleInputAuthorChange = this.handleInputAuthorChange.bind(this);
+    this.handleInputGenreChange = this.handleInputGenreChange.bind(this);
+    this.handleQuoteSubmit = this.handleQuoteSubmit.bind(this);
   }
 
-  /* triggered before rendering, but will be overrritten by "didMount" */
-  componentWillMount() {
-    console.log('APP will mount');
-    this.setState({
-      bgColor: 'red',
-    })
-  }
-
-  /*
-  is rendered last right before rendering
-  hence we should do last state manipulations here
-  */
+   // INITIATE FETCHING WHEN COMPONENTS LOADS.
   componentDidMount() {
-    console.log('APP did mount');
-    axios('https://dresselhaus-quotes-api.herokuapp.com/api/quotes')
-      .then(res => {
-        this.setState({
-          apiData: res.data.quotesData,
-          bgColor: '#f3d8de',
-        })
-      })
-  }
+    axios('https://ada-api.herokuapp.com/api/quotes')
+      .then(res =>{
+        this.setState(prevState => {
+          return {
+            quotes: res.data.quotesData,
+          }
+        });
+      });
+    }
+
+  //COLOR FUN
   changeBgColor() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
+    let letters= '0123456789ABCDEF';
+    let coor = '#';
+    for(let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
-    this.setState({
-      bgColor: color,
-    })
-  }
+     this.setState ({
+      bgColor:color,
+     })
+   }
 
-  render() {
+   // CONTROLLED INPUT FIELDS (RECCOMENDED)
+
+   handleInputContentChange(event) {
+    this.setState({
+      inputContentValue: event.target.value
+    });
+   }
+
+   handleInputGenreChange(event) {
+    this.setState({
+      inputGenreValue: event.target.value
+    });
+   }
+
+   handleQuoteSubmit(event) {
+    event.preventDefault();
+    event.target.content = '';
+
+    axios.post('https://ada-api.herokuapp.com/api/quotes', {
+      content: this.state.inputContentValue,
+      author: this.state.inputAuthorValue,
+      genre_type: this.state.inputGenreValue
+    })
+    .then(res => {
+      console.log(res.data.quote);
+      if (res.data.quote.id !== undefined) {
+        const newQuote = {
+          content: res.data.quote.content,
+          author: res.data.quote.author,
+          genre_type: res.data.quote.genre_type,
+        }
+        this.setState((prevState) => {
+          return {
+            quotes: prevState.quotes.concat(newQuote),
+          }
+        })
+       }
+     }) .catch(err => console.log(err));
+   }
+
+   render() {
     console.log('APP rendering', this.state);
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2> Welcome to Dresselhaus Quotes </h2>
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h2> Quotes App Part Deux </h2>
         </div>
+        <AddQuoteForm
+          handleQuoteSubmit={this.handleQuoteSubmit}
+          handleInputContentChange={this.handleInputContentChange}
+          handleInputAuthorChange={this.handleInputAuthorChange}
+          handleInputGenreChange={this.handleInputGenreChange}
+          inputContentValue={this.state.inputContentValue}
+          inputAuthorValue={this.state.inputAuthorValue}
+          inputGenreValue={this.state.inputGenreValue}
+        />
         <QuotesList
-            hello="Hello, "
-            data={this.state.apiData}
-            bgColor={this.state.bgColor}
-            changeBgColor={this.changeBgColor} />
+          data={this.state.quotes}
+          bgColor={this.state.bgColor}
+          changeBgColor={this.changeBgColor}
+          />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
--- quotes-form-start/src/components/QuotesList.js
+++ quotes-form-start/src/components/QuotesList.js
@@ -3,37 +3,14 @@ import Quote from './Quote';
 
 class QuotesList extends Component {
 
-  componentWillReceiveProps(nextProps) {
-    console.log('QL will receive props');
-  }
-
-  componentWillMount() {
-    console.log('QL will mount');
-    /* stop from */
-    return false;
-  }
-
-  /* is triggered right after the state change */
-  componentWillUpdate() {
-    console.log('QL will update');
-  }
-  /*
-  triggered right after rendering
-  use case: posting to database after the form submisison
-  */
-  componentDidUpdate() {
-    console.log('QL did update');
-  }
 
   render() {
     console.log('QL rendering');
     return (
       <div style={{ backgroundColor: this.props.bgColor }}>
-        {this.props.hello} {this.props.bgColor} color!
-        <button onClick={this.props.changeBgColor}>
-          Change Color !</button>
-        { this.props.data.map(quote => {
-          return <Quote quote={quote} key={quote.id}/>
+        <button id='color-change'onClick={this.props.changeBgColor}> {this.props.bgColor} Change Color ! </button>
+        { this.props.data.map( quote => {
+          return <Quote quote={quote} key={quote.id} />
         })}
       </div>
     )
